bbfd301a732bd329e00e27eb47cea72849c59959
armantuto <armantuto1992@gmail.com>
2023-10-12 21:23:16 +0200

    a

--- JAVASCRIPT/tasse.js
+++ JAVASCRIPT/tasse.js
@@ -35,9 +35,10 @@ function CalcolaTasse() {
     var inpsP = 26.23;
     var tasassione = 67;
     var error = "controlla i dati inseriti"
+    var totale = 0
 
-    if (lordeM >= 1, carburante >= "", altreSpesse >= "") {
-        var tasassioneP = parseFloat(lordeM) * parseFloat(tasassione) / 100
+    if (lordeM >= 0, carburante >= 0, altreSpesse >= 0) {
+        var tasassioneP = parseFloat(lordeM) * tasassione / 100
         var iva = parseFloat(tasassioneP * ivaP) / 100
         var inps = parseFloat(tasassioneP) * parseFloat(inpsP) / 100
         var totaleSpesse = parseFloat(carburante) + parseFloat(altreSpesse)
@@ -47,7 +48,7 @@ function CalcolaTasse() {
 
 
         // document.getElementById("nam").innerText = nombre + " " + cognome
-        document.getElementById("calcola").innerText = "Totale netto dopo spese e tasse:   €"+totale
+        document.getElementById("calcola").innerText = "Totale netto dopo spese e tasse:   €" + totale
     }
     else {
         document.getElementById("calcola").innerText = error
@@ -63,7 +64,7 @@ function NettoConsegna(){
     var inps2 = 26.23
 
     if (priceCarburante >= 0, priceConsegna <= 0, kmConsegna >=0 ){
-        var carburanteNetto = parseFloat(priceCarburante / 35) * kmConsegna;
+        var carburanteNetto = parseFloat(priceCarburante / 35) *  kmConsegna;
         var consegnaFinal = parseFloat(priceConsegna * 67) / 100
         var I = parseFloat(consegnaFinal * iva2) / 100
         var IN = parseFloat(consegnaFinal * inps2) / 100
@@ -77,6 +78,8 @@ function NettoConsegna(){
         document.getElementById("benzi").innerText = "Totale spesso in benzina per questa corsa:"  + " €" + carburanteNetto.toFixed(2)
         document.getElementById("tas").innerText ="Totale tasse  per questa corsa:"  +  " €" + a.toFixed(2)
         
+    }else{
+        document.getElementById("calcola").innerText= error
     }
 
 }
